a3acec9abd81289fe9c48b2e075d23cc748cb78c
Alexander Hartson <alexander-hartson@live.com>
2018-05-14 22:30:32 -0500

    adding files

---
+++ src/examples/pubhub/dao/TagDAO.java
@@ -0,0 +1,16 @@
+package examples.pubhub.dao;
+
+import java.util.List;
+
+import examples.pubhub.model.Tag;
+
+/**
+ * Interface for our Data Access Object to handle database queries related to Books.
+ */
+public interface TagDAO {
+	public List<Tag> getAllTags(String tag);
+	public Tag getTagByISBN(String isbn);
+	
+	public boolean addTag(Tag tag);
+	public boolean updateTag(Tag tag);
+}
---
+++ src/examples/pubhub/dao/TagDAOImpl.java
@@ -0,0 +1,36 @@
+package examples.pubhub.dao;
+
+import java.util.List;
+
+import examples.pubhub.model.Tag;
+
+/**
+ * Implementation for the BookDAO, responsible for querying the database for Book objects.
+ */
+public class TagDAOImpl implements TagDAO {
+
+	@Override
+	public List<Tag> getAllTags(String tag) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Tag getTagByISBN(String isbn) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean addTag(Tag tag) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean updateTag(Tag tag) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
